c11ee4387f0e96dd954f4a0bd3489194d82c1c1b
Andy608 <AndrewR608@gmail.com>
2018-11-08 01:36:32 -0500

    Added in ring animation.

---
+++ Pocket Planets/Assets/Scripts/Game/Planet/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 30.0f;
+    private Transform objectTransform;
+
+    private void Start()
+    {
+        objectTransform = transform;
+    }
+
+    private void Update()
+    {
+        objectTransform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
