f8e5b45f99ebeab0888d6762cd064a9bced2f74f
Andrew Rimpici <andrew.rimpici@mymail.champlain.edu>
2019-05-19 17:37:54 -0400

    Resized window to give more room on desktop computers.

--- Pocket Planets/Assets/Scripts/Managers/Persistent/StartupResize.cs
+++ Pocket Planets/Assets/Scripts/Managers/Persistent/StartupResize.cs
@@ -7,6 +7,6 @@ public class StartupResize : MonoBehaviour
 	void Start ()
     {
         //Screen.SetResolution(443, 960, FullScreenMode.Windowed, 60);
-        Screen.SetResolution(540, 960, FullScreenMode.Windowed, 60);
+        //Screen.SetResolution(540, 960, FullScreenMode.Windowed, 60);
     }
 }
